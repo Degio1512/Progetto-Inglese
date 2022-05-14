93c12b125543274ee359a1c5a9ab179800c321e1
Degio1512 <Lorenzodegiorgi2004@gmail.com>
2022-05-14 18:21:14 +0200

    update

--- src/script/tipying-effect.js
+++ src/script/tipying-effect.js
@@ -1,6 +1,6 @@
 var i=0
 var txt='Project For Revise all the argument of the year. The site is divided in two part, the first that is about the history of the year and the second is about the litterature so about the poets and texts of the year'
-var speed=500
+var speed=80
 function type(){
     document.getElementById('type').innerHTML
         +=txt.charAt(i);
